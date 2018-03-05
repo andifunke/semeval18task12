3babc2c1872dcdb265cd89335530ee29c269a9e9
Andreas Funke <andreas.funke@uni-duesseldorf.de>
2018-03-05 01:39:24 +0100

    ensemble plots

--- src/ensemble_voting.py
+++ src/ensemble_voting.py
@@ -1,6 +1,7 @@
 """ combining predictions based on votes by a set of answer files """
 import pandas as pd
 import numpy as np
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from sklearn.metrics import accuracy_score
 from os import listdir
 import re
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 from constants import LABEL
 from preprocessing import get_train_dev_test
 from results_evaluator import tprint
+import seaborn as sns
 
 
 def combine(y_true, y_pred, conf):
@@ -29,30 +31,75 @@ def combine(y_true, y_pred, conf):
     return acc_conf, acc_major
 
 
-def plot(df, name, show=True, save=False):
+def plot_axis(df, ax, legend_pos='orig1'):
+    df.plot(x=np.arange(1, len(df) + 1), ax=ax, use_index=False, xlim=[-25, len(df) + 25], ylim=[0.5, 0.775],
+            style=['-', '-', '-', '-'], lw=1.5,
+            yticks=[.5, .525, .55, .575, .6, .625, .65, .675, .7, .725, .75, .775])
+    ax.lines[1].set_linewidth(0.9)  # 1.15
+    ax.lines[3].set_linewidth(0.9)  # 1.15
+    col1 = ax.lines[0].get_color()
+    col2 = ax.lines[2].get_color()
+    ax.lines[1].set_color(tuple(1.3*c for c in col1))  # 1.1*
+    ax.lines[3].set_color(tuple(1.3*c for c in col2))  # 1.1*
 
-    tprint(df, 5)
+    ax.grid(b=True, which='major', linestyle='-', linewidth=0.85)
+    ax.grid(b=True, which='minor', linestyle=':', linewidth=0.75)
 
-    fig, ax = plt.subplots()
-    df.plot(ax=ax)
-    ax.yaxis.grid()
-    # ax.set_ylim(.5, .8)
-    ax.set_xlabel('number of models')
-    ax.set_ylabel('accuracy')
-    ax.set_title('Ensemble scores')
+    if legend_pos == 'orig1':
+        ax.legend(loc='center', bbox_to_anchor=(0.5, 0.365))
+    elif legend_pos == 'orig2':
+        ax.legend().remove()
+    elif legend_pos == 'alt1':
+        ax.legend(loc='center', bbox_to_anchor=(0.5, 0.14))
+    elif legend_pos == 'alt2':
+        ax.legend(loc='lower left', bbox_to_anchor=(0.02, 0))
+    else:
+        ax.legend()
 
-    # ax2 = ax1.twiny()
+    ax.set_xlabel('number of models', weight='bold')
+    ax.set_ylabel('accuracy', weight='bold')
 
-    # xtick_range = np.arange(0, len(df), len(df)//16)
-    # xticks = x.iloc[xtick_range]
-    # xticks = np.round(xticks, 3)
-    # ax2.set_xticks(xtick_range)
-    # ax2.set_xticklabels(xticks)
+    # majorLocator_x = MultipleLocator(500)
+    majorLocator_y = MultipleLocator(.05)
+    majorFormatter_y = FormatStrFormatter('%.2f')
+    minorLocator_y = MultipleLocator(.025)
 
+    # ax.xaxis.set_major_locator(majorLocator_x)
+    ax.yaxis.set_major_locator(majorLocator_y)
+    ax.yaxis.set_major_formatter(majorFormatter_y)
+
+    # for the minor ticks, use no labels; default NullFormatter
+    ax.yaxis.set_minor_locator(minorLocator_y)
+
+
+def plot_figure(dfs: list, name, show=True, save=False, legend_pos: list=None, align='h'):
+    length = len(dfs)
+    if legend_pos is None:
+        legend_pos = [''] * length
+
+    sns.set(color_codes=True, font_scale=1)
+    sns.set_style("whitegrid", {'legend.frameon': True})
+    sns.set_palette("deep")
+
+    if align == 'h':
+        fig, ax = plt.subplots(ncols=length, figsize=(5*length, 5), sharey=True)
+    else:
+        fig, ax = plt.subplots(nrows=length, figsize=(5, 5*length))
+
+    if length > 1:
+        for i, df in enumerate(dfs):
+            plot_axis(df, ax[i], legend_pos=legend_pos[i])
+        ax[0].set_title('original dataset')
+        ax[1].set_title('alternative (randomized) data split')
+    else:
+        plot_axis(dfs[0], ax, legend_pos=legend_pos[0])
+
+    fig.tight_layout()
     if show:
         plt.show()
     if save:
-        fig.savefig('../figures/' + name + '.pdf')
+        fig.savefig(name + '.pdf', bbox_inches='tight')
+    plt.close('all')
 
 
 def build_df(files, y_true):
@@ -68,11 +115,11 @@ def build_df(files, y_true):
 
 def main():
     names = {
-        'tensorL05con2redo2': '/media/andreas/Linux_Data/hpc-semeval/tensorL05con2redo2/out/',
-        # 'alt_split_odd': '/media/andreas/Linux_Data/hpc-semeval/alt_split_odd/out/',
+        # 'tensorL05con2redo2': '/media/andreas/Linux_Data/hpc-semeval/tensorL05con2redo2/out/',
+        'alt_split_odd': '/media/andreas/Linux_Data/hpc-semeval/alt_split_odd/out/',
     }
 
-    _, df_dev_data, df_tst_data = get_train_dev_test(options=dict(alt_split=False))
+    _, df_dev_data, df_tst_data = get_train_dev_test(options=dict(alt_split=True))
 
     dev_true = df_dev_data[LABEL].values.flatten()
     tst_true = df_tst_data[LABEL].values.flatten()
@@ -124,18 +171,25 @@ def main():
             tst_major_scores.append(acc_major_tst)
 
         mtrx = {
-            'dev: confidence vote': dev_conf_scores,
-            'test: confidence vote': tst_conf_scores,
-            'dev: majority vote': dev_major_scores,
-            'test: majority vore': tst_major_scores,
-            # 'dev: mean accuracy': dev_mean,
+            # 'dev: confidence vote': dev_conf_scores,
+            # 'test: confidence vote': tst_conf_scores,
             'test: mean accuracy': tst_mean,
             'dev: sorted accuracy': df['dev', 'acc', 0],
+            'dev: majority vote': dev_major_scores,
+            'test: majority vote': tst_major_scores,
+            # 'dev: mean accuracy': dev_mean,
         }
 
         df = pd.DataFrame(mtrx)
-        plot(df, k + 'all_')
+        plot_figure([df], k + 'all_')
+        # df.to_csv('../figures/alt-split.csv', sep='\t')
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    df1 = pd.read_csv('../out/orig-split.csv', sep='\t')
+    df2 = pd.read_csv('../out/alt-split.csv', sep='\t')
+    plot_figure([df1], '../out/orig-split_2', save=True, legend_pos=['orig1'])
+    plot_figure([df2], '../out/alt-split_2', save=True, legend_pos=['alt1'])
+    plot_figure([df1, df2], '../out/ensemble_h_2', save=True, legend_pos=['orig2', 'alt2'], align='h')
+    plot_figure([df1, df2], '../out/ensemble_v_2', save=True, legend_pos=['orig2', 'alt2'], align='v')
